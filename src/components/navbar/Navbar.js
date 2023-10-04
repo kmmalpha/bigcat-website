7a7fb2bc635935464cf5b27b418137cdabadc755
kmmalpha <keithndlovu4@gmail.com>
2023-10-04 15:57:15 -0700

    made more changes to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,25 @@
+import { React, useState } from 'react';
 import '../styles/Navbar.css';
 import { NavLink } from 'react-router-dom';
+import Hamburger from 'hamburger-react'
 
 const Navbar = () => {
+	const [showNavbar, setShowNavbar] = useState(false)
+
+	const toggleNavbar = () => {
+		setShowNavbar(!showNavbar)
+	}
+
     return (
 		<nav className="navbar">
 			<div className="container">
 				{/* <div className="logo">
 					<img src="src/images/New Logo.png" />
 				</div> */}
-				<div className="nav-elements">
+				{/* <div className='menu-icon' onClick={toggleNavbar}>
+					<Hamburger />
+				</div> */}
+				<div className={`nav-elements ${showNavbar && 'active'}`}>
 					<ul>
 						<li>
 							<NavLink to="/maaya">MAAYA</NavLink>
@@ -17,7 +28,7 @@ const Navbar = () => {
 							<NavLink to="/music-in-green">MUSIC IN GREEN</NavLink>
 						</li>
 						<li>
-							<NavLink to="/"><img src="src/images/New Logo.png" alt="bcg-logo" /></NavLink>
+							<NavLink to="/"><img src="src/images/New Logo.png" alt="bcg-logo" className="logo" /></NavLink>
 						</li>
 						<li>
 							<NavLink to="/gallery">GALLERY</NavLink>
